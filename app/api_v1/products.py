f555e9216db32c5226251cb31567bbe23462516d
Jonathan Sick <jsick@lsst.org>
2016-02-09 16:25:53 -0700

    Outline HTTP API reference documentation
    This commit adds a common structure for documenting the core resources
    provided by the LTD Keeper api: /products/, /builds/ and /versions/.
    
    The idea is to
    
    - introduce each concept briefly
    - provide a summary list of methods (and point out related methods under
      other routes
    - use httpdomain's autoflask to provide full documentation for each
      method
    
    More effort is needed to add examples and metadata to the docstrings;
    see
    http://pythonhosted.org/sphinxcontrib-httpdomain/#roles
    for info on adding structured data to the docstrings of routes.
    
    For DM-5100.

--- app/api_v1/products.py
+++ app/api_v1/products.py
@@ -9,21 +9,38 @@ from ..models import Product
 
 @api.route('/products/', methods=['GET'])
 def get_products():
-    """GET listing of all products."""
+    """List all documentation products (anonymous access allowed).
+
+    **Example request**
+
+    .. code-block:: http
+
+        GET /v1/products/ HTTP/1.1
+
+    **Example response**
+
+    .. code-block:: http
+
+        HTTP/1.0 200 OK
+
+    :reqheader Authorization: ``<token>:``
+    """
     return jsonify({'products': [product.get_url() for product in
                                  Product.query.all()]})
 
 
 @api.route('/products/<int:id>', methods=['GET'])
 def get_product(id):
-    """GET single product by table row id."""
+    """Get the record of a single documentation product (anonymous access
+    allowed).
+    """
     return jsonify(Product.query.get_or_404(id).export_data())
 
 
 @api.route('/products/', methods=['POST'])
 @token_auth.login_required
 def new_product():
-    """POST a new product. Token authentication required."""
+    """Create a new documentation product (token required)."""
     product = Product()
     product.import_data(request.json)
     db.session.add(product)
@@ -34,7 +51,7 @@ def new_product():
 @api.route('/products/<int:id>', methods=['PUT'])
 @token_auth.login_required
 def edit_product(id):
-    """PUT changes to a product. Token authentication required."""
+    """Update a product (token required)."""
     product = Product.query.get_or_404(id)
     product.import_data(request.json)
     db.session.add(product)
--- docs/index.rst
+++ docs/index.rst
@@ -1,11 +1,18 @@
 ##########
-LTD-Keeper
+LTD Keeper
 ##########
 
-Contents
-========
+LTD Keeper is the backend database and application that runs LSST the Docs.
+You can interact with the Keeper through its RESTful HTTP API.
+
+For more information about LSST the Docs, see `SQR-006: Documentation Deployment Service for LSST's Eups-based Software <http://sqr-006.lsst.io>`_.
 
 .. toctree::
-   :maxdepth: 2
+   :caption: HTTP API
+   :name: http-api
+   :maxdepth: 1
 
    auth
+   products
+   builds
+   editions
