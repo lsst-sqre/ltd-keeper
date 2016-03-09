cc819dc261117ae5bfaf2250654097e1557040e8
Jonathan Sick <jsick@lsst.org>
2016-03-09 13:33:36 -0700

    doc_example_driver.sh now flexible in output
    The output now goes to stdout and there are instructions to redirect to
    a file as necessary.
    
    Also includes instruction enhancements:
    - install httpie
    - run.py --dev

--- integration_tests/doc_example_driver.sh
+++ integration_tests/doc_example_driver.sh
@@ -3,19 +3,21 @@
 # This script drives httpie to interact with a local development ltd-keeper
 # instance. The output can be be used for examples in the docs.
 #
+# Get httpie from http://httpie.org or install:
+# pip install httpie
+#
 # Development server prep
 # 1. Delete any existing ltd-keeper-dev.sqlite
-# 2. Run the dev server ./run.py
+# 2. Run the dev server ./run.py --dev
 # 3. Get an auth token
 #        http --auth user:pass GET http://localhost/token
 #        export KEEPER_TOKEN=<token>
-# In a separate session, run ./doc_example_driver.sh
-# Shut down the dev server and clean-up and sqlite DB
+#    ('user' and 'pass' are dev mode defaults)
+#
+# In a separate session, run
+#    ./doc_example_driver.sh &> examples.txt
 
-if [ -e "ltd_keeper_doc_examples.txt" ]; then
-    rm "ltd_keeper_doc_examples.txt"
-    touch "ltd_keeper_doc_examples.txt"
-fi
+# Finally, shut down the dev server and clean-up and sqlite DB
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
     POST http://localhost:5000/products/ \
@@ -23,10 +25,9 @@ http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
     doc_repo="https://github.com/lsst/pipelines_docs.git" \
     domain="pipelines.lsst.io" \
     slug="lsst_apps" \
-    title="LSST Science Pipelines" \
-    >> ltd_keeper_doc_examples.txt
+    title="LSST Science Pipelines"
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
     POST http://localhost:5000/products/ \
@@ -34,65 +35,57 @@ http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
     doc_repo="https://github.com/lsst/qserv.git" \
     domain="qserv.lsst.io" \
     slug="qserv_distrib" \
-    title="Qserv" \
-    >> ltd_keeper_doc_examples.txt
+    title="Qserv"
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
     GET http://localhost:5000/products/lsst_apps \
     >> ltd_keeper_doc_examples.txt
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
-    GET http://localhost:5000/products/ \
-    >> ltd_keeper_doc_examples.txt
+    GET http://localhost:5000/products/
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
     PATCH http://localhost:5000/products/qserv_distrib \
-    title="Qserv Data Access" \
-    >> ltd_keeper_doc_examples.txt
+    title="Qserv Data Access"
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
     POST http://localhost:5000/products/lsst_apps/builds/ \
     slug='b1' \
     github_requester='jonathansick' \
-    git_refs:='["master"]' \
-    >> ltd_keeper_doc_examples.txt
+    git_refs:='["master"]'
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
     POST http://localhost:5000/products/lsst_apps/builds/ \
     slug='b2' \
     github_requester='jonathansick' \
-    git_refs:='["master"]' \
-    >> ltd_keeper_doc_examples.txt
+    git_refs:='["master"]'
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
-
-http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
-    POST http://localhost:5000/builds/1/uploaded \
-    >> ltd_keeper_doc_examples.txt
-
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
-    GET http://localhost:5000/products/lsst_apps/builds/ \
-    >> ltd_keeper_doc_examples.txt
+    POST http://localhost:5000/builds/1/uploaded
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
-    GET http://localhost:5000/builds/1 \
-    >> ltd_keeper_doc_examples.txt
+    GET http://localhost:5000/products/lsst_apps/builds/
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
+
+http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
+    GET http://localhost:5000/builds/1
+
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
     POST http://localhost:5000/products/lsst_apps/editions/ \
@@ -100,43 +93,36 @@ http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
     title="Latest" \
     build_url="http://localhost:5000/builds/1" \
     tracked_refs:='["master"]' \
-    published_url="pipelines.lsst.io" \
-    >> ltd_keeper_doc_examples.txt
+    published_url="pipelines.lsst.io"
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
     PATCH http://localhost:5000/editions/1 \
-    title="Development master" \
-    >> ltd_keeper_doc_examples.txt
+    title="Development master"
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
-
-http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
-    GET http://localhost:5000/editions/1 \
-    >> ltd_keeper_doc_examples.txt
-
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
-    GET http://localhost:5000/products/lsst_apps/editions/ \
-    >> ltd_keeper_doc_examples.txt
+    GET http://localhost:5000/editions/1
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
+
+http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
+    GET http://localhost:5000/products/lsst_apps/editions/
+
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
     POST http://localhost:5000/editions/1/rebuild \
-    build_url="http://localhost:5000/builds/2" \
-    >> ltd_keeper_doc_examples.txt
+    build_url="http://localhost:5000/builds/2"
 
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
-
-http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
-    DELETE http://localhost:5000/builds/1 \
-    >> ltd_keeper_doc_examples.txt
-
-echo "\n\n---\n" >> ltd_keeper_doc_examples.txt
+echo "\n\n---\n"
 
 http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
-    DELETE http://localhost:5000/editions/1 \
-    >> ltd_keeper_doc_examples.txt
+    DELETE http://localhost:5000/builds/1
+
+echo "\n\n---\n"
+
+http -p hbHB --pretty=format --auth $KEEPER_TOKEN: \
+    DELETE http://localhost:5000/editions/1
