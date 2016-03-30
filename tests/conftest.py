8416999b37a06a145e9ba6efcf20a83c4fe1bdd1
Jonathan Sick <jsick@lsst.org>
2016-03-29 23:51:30 -0700

    Add test clients with different permissions
    The existing default 'client' is setup to have full permissions so it
    can be used in functionality tests; these
    
    The new clients have specific test individual permission bits assigned
    to their corresponding test user account.
    
    There is also an anonymous test client to test that use case.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,18 @@
 import pytest
 
 from app import create_app, db
-from app.models import User
+from app.models import User, Permission
 from app.testutils import TestClient
 
 
 DEFAULT_USERNAME = 'hipster'
 DEFAULT_PASSWORD = 'pug'
+
+# additional usernames with specific roles
+PRODUCT_ADMIN_USERNAME = 'product_admin'
+EDITION_ADMIN_USERNAME = 'edition_admin'
+BUILD_UPLOADER_USERNAME = 'build_uploader'
+BUILD_DEPRECATOR_USERNAME = 'build_deprecator'
 
 
 @pytest.fixture
@@ -19,9 +25,33 @@ def empty_app(request):
     ctx.push()
     db.drop_all()
     db.create_all()
-    u = User(username=DEFAULT_USERNAME)
+
+    # Creates users with each of the permissions
+    u = User(username=DEFAULT_USERNAME,
+             permissions=Permission.full_permissions())
     u.set_password(DEFAULT_PASSWORD)
     db.session.add(u)
+
+    u = User(username=PRODUCT_ADMIN_USERNAME,
+             permissions=Permission.ADMIN_PRODUCT)
+    u.set_password(DEFAULT_PASSWORD)
+    db.session.add(u)
+
+    u = User(username=EDITION_ADMIN_USERNAME,
+             permissions=Permission.ADMIN_EDITION)
+    u.set_password(DEFAULT_PASSWORD)
+    db.session.add(u)
+
+    u = User(username=BUILD_UPLOADER_USERNAME,
+             permissions=Permission.UPLOAD_BUILD)
+    u.set_password(DEFAULT_PASSWORD)
+    db.session.add(u)
+
+    u = User(username=BUILD_DEPRECATOR_USERNAME,
+             permissions=Permission.DEPRECATE_BUILD)
+    u.set_password(DEFAULT_PASSWORD)
+    db.session.add(u)
+
     db.session.commit()
 
     def fin():
@@ -44,6 +74,49 @@ def basic_client(empty_app):
 def client(empty_app):
     """Client with token-based auth, using the `app` application."""
     _c = TestClient(empty_app, DEFAULT_USERNAME, DEFAULT_PASSWORD)
+    r = _c.get('/token')
+    client = TestClient(empty_app, r.json['token'])
+    return client
+
+
+@pytest.fixture
+def anon_client(empty_app):
+    """Anonymous client."""
+    client = TestClient(empty_app, '', '')
+    return client
+
+
+@pytest.fixture
+def product_client(empty_app):
+    """Client with token-based auth with ADMIN_PRODUCT permissions."""
+    _c = TestClient(empty_app, PRODUCT_ADMIN_USERNAME, DEFAULT_PASSWORD)
+    r = _c.get('/token')
+    client = TestClient(empty_app, r.json['token'])
+    return client
+
+
+@pytest.fixture
+def edition_client(empty_app):
+    """Client with token-based auth with ADMIN_EDITION permissions."""
+    _c = TestClient(empty_app, EDITION_ADMIN_USERNAME, DEFAULT_PASSWORD)
+    r = _c.get('/token')
+    client = TestClient(empty_app, r.json['token'])
+    return client
+
+
+@pytest.fixture
+def upload_build_client(empty_app):
+    """Client with token-based auth with UPLOAD_BUILD permissions."""
+    _c = TestClient(empty_app, BUILD_UPLOADER_USERNAME, DEFAULT_PASSWORD)
+    r = _c.get('/token')
+    client = TestClient(empty_app, r.json['token'])
+    return client
+
+
+@pytest.fixture
+def deprecate_build_client(empty_app):
+    """Client with token-based auth with DEPRECATE_BUILD permissions."""
+    _c = TestClient(empty_app, BUILD_DEPRECATOR_USERNAME, DEFAULT_PASSWORD)
     r = _c.get('/token')
     client = TestClient(empty_app, r.json['token'])
     return client
