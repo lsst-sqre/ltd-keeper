4da09252f392c73568c75071dd39429cfaa56835
Jonathan Sick <jsick@lsst.org>
2016-03-31 13:45:27 -0700

    Transition to class-based config
    By maintaining configs in an object it's possible for
    environment-specific configuration profiles to inherit
    configurations from a base class.
    
    Following the pattern in
    https://github.com/miguelgrinberg/flasky/blob/master/app/__init__.py
    we can also offload profile-specifc init code to the init_app sub
    classmethods (in the example these class methods are used to set up
    logging).
    
    The Production config profile is intended to draw almost entirely from
    environment variables.

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,10 @@ Copyright 2016 AURA/LSST.
 Copyright 2014 Miguel Grinberg.
 """
 
-import os
 from flask import Flask, jsonify, g
 from flask.ext.sqlalchemy import SQLAlchemy
+
+from config import config
 
 db = SQLAlchemy()
 
@@ -24,9 +25,7 @@ def create_app(config_name='production'):
     app = Flask(__name__)
 
     # apply configuration
-    _this_dir = os.path.dirname(os.path.abspath(__file__))
-    cfg = os.path.join(_this_dir, '../config', config_name + '.py')
-    app.config.from_pyfile(cfg)
+    app.config.from_object(config[profile])
 
     # initialize extensions
     db.init_app(app)
---
+++ config.py
@@ -0,0 +1,69 @@
+"""ltd-keeper configuration and environment profiles."""
+
+import abc
+import os
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    """Configuration baseclass."""
+
+    __metaclass__ = abc.ABCMeta
+
+    SECRET_KEY = 'secret-key'
+    DEBUG = False
+    IGNORE_AUTH = False
+
+    @abc.abstractclassmethod
+    def init_app(cls, app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    """Local development configuration."""
+
+    DEBUG = True
+    IGNORE_AUTH = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('LTD_KEEPER_DEV_DB_URL') or \
+        'sqlite:///' + os.path.join(BASEDIR, 'ltd-keeper-dev.sqlite')
+    DEFAULT_USER = 'user'
+    DEFAULT_PASSWORD = 'pass'
+
+    @classmethod
+    def init_app(cls, app):
+        pass
+
+
+class TestConfig(Config):
+    """Test configuration (for py.test harness)."""
+
+    SERVER_NAME = 'testing.io'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' \
+        + os.path.join(BASEDIR, 'ltd-keeper-test.sqlite')
+    SERVER_NAME = 'testing.io'
+
+    @classmethod
+    def init_app(cls, app):
+        pass
+
+
+class ProductionConfig(Config):
+    """Production configuration."""
+
+    SECRET_KEY = os.environ.get('LTD_KEEPER_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('LTD_KEEPER_DB_URL')
+    DEFAULT_USER = os.environ.get('LTD_KEEPER_BOOTSTRAP_USER')
+    DEFAULT_PASSWORD = os.environ.get('LTD_KEEPER_BOOTSTRAP_PASSWORD')
+
+    @classmethod
+    def init_app(cls, app):
+        pass
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestConfig,
+    'production': ProductionConfig,
+    'default': ProductionConfig
+}
