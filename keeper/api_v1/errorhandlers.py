b51786bce17d10dbeb15ecf218b7e8cc63c0a0c5
Jonathan Sick <jsick@lsst.org>
2017-12-13 15:47:13 -0700

    Add loggers for 400-series error handlers
    It seems that Flask bypasses the regular close-out of the log_route
    decorator, so its necessary to log the status here in the error
    handlers.

--- keeper/api_v1/errorhandlers.py
+++ keeper/api_v1/errorhandlers.py
@@ -15,6 +15,9 @@ from . import api
 @api.errorhandler(ValidationError)
 def bad_request(e):
     """Handler for ValidationError exceptions."""
+    logger = structlog.get_logger()
+    logger.error(status=400, message=e.args[0])
+
     response = jsonify({'status': 400, 'error': 'bad request',
                         'message': e.args[0]})
     response.status_code = 400
@@ -24,6 +27,9 @@ def bad_request(e):
 @api.app_errorhandler(404)
 def not_found(e):
     """App-wide handler for HTTP 404 errors."""
+    logger = structlog.get_logger()
+    logger.error(status=400)
+
     response = jsonify({'status': 404, 'error': 'not found',
                         'message': 'invalid resource URI'})
     response.status_code = 404
@@ -33,6 +39,9 @@ def not_found(e):
 @api.errorhandler(405)
 def method_not_supported(e):
     """Handler for HTTP 405 exceptions."""
+    logger = structlog.get_logger()
+    logger.error(status=405)
+
     response = jsonify({'status': 405, 'error': 'method not supported',
                         'message': 'the method is not supported'})
     response.status_code = 405
