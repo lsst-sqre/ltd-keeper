dbeb6c4087c0eb05128d3cbf7502be6a2f7e1bae
Jonathan Sick <jsick@lsst.org>
2018-04-18 17:52:32 -0700

    Refactor get_auth_token from the app factory
    It makes sense for get_auth_token to be part of the blueprint since
    there shouldn't be anything exceptional about this route. Now the app
    factory is a lot cleaner.

--- docs/auth.rst
+++ docs/auth.rst
@@ -3,4 +3,4 @@ Authentication - `/token`
 #########################
 
 .. autoflask:: keeper:flask_app
-   :endpoints: get_auth_token
+   :endpoints: api.get_auth_token
--- keeper/appfactory.py
+++ keeper/appfactory.py
@@ -11,10 +11,9 @@ __all__ = ('create_flask_app',)
 
 import os
 
-from flask import Flask, jsonify, g
+from flask import Flask
 
 from .config import config
-from .logutils import log_route
 
 
 def create_flask_app(profile=None):
@@ -22,9 +21,6 @@ def create_flask_app(profile=None):
 
     This is called by a runner script, such as /run.py.
     """
-
-    from .auth import password_auth
-
     app = Flask('keeper')
 
     # Apply configuration
@@ -42,48 +38,5 @@ def create_flask_app(profile=None):
     # Register blueprints
     from .api_v1 import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix=None)
-
-    # authentication token route
-    @app.route('/token')
-    @log_route()
-    @password_auth.login_required
-    def get_auth_token():
-        """Obtain a token for API users.
-
-        **Example request**
-
-        .. code-block:: http
-
-           GET /token HTTP/1.1
-           Accept: */*
-           Accept-Encoding: gzip, deflate
-           Authorization: Basic dXNlcjpwYXNz
-           Connection: keep-alive
-           Host: localhost:5000
-           User-Agent: HTTPie/0.9.3
-
-        **Example response**
-
-        .. code-block:: http
-
-           HTTP/1.0 200 OK
-           Content-Length: 139
-           Content-Type: application/json
-           Date: Tue, 09 Feb 2016 20:23:11 GMT
-           Server: Werkzeug/0.11.3 Python/3.5.0
-
-           {
-               "token": "eyJhbGciOiJIUzI1NIsImlhdCI6MTQ1NTA0OTM5MSwiZXhwIjo..."
-           }
-
-        :reqheader Authorization: ``username:password``
-
-        :>json string token: Token string. Use this token in the basic auth
-           ``username`` field.
-
-        :statuscode 200: No errors.
-        :statuscode 401: Not authenticated.
-        """
-        return jsonify({'token': g.user.generate_auth_token()})
 
     return app
