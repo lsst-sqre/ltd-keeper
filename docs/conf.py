6e9c86eb03077124ef39f3dbf5b2f5f04538cb8a
Jonathan Sick <jsick@lsst.org>
2020-03-20 09:57:55 -0400

    Update copyright for 2020

--- docs/conf.py
+++ docs/conf.py
@@ -56,8 +56,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'LTD Keeper'
-copyright = '2016–2019, Association of Universities for Research in Astronomy'
-author = 'Association of Universities for Research in Astronomy'
+copyright = '2016–2020, Association of Universities for Research in Astronomy, Inc. (AURA)'
+author = 'Association of Universities for Research in Astronomy, Inc. (AURA)'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
