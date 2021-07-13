08e86e4642a3f4165c4408b718961591fa3d6476
Jonathan Sick <jonathan@jsick.codes>
2021-07-13 11:57:32 -0400

    Add pydantic model for root endpoint

--- keeper/api/_models.py
+++ keeper/api/_models.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from pydantic import BaseModel, SecretStr
+from pydantic import BaseModel, HttpUrl, SecretStr
 
 
 class AuthTokenResponse(BaseModel):
@@ -15,3 +15,39 @@ class AuthTokenResponse(BaseModel):
         json_encoders = {
             SecretStr: lambda v: v.get_secret_value() if v else None,
         }
+
+
+class RootLinks(BaseModel):
+    """Sub-resource containing links to APIs."""
+
+    self: HttpUrl
+    """The URL of this resource."""
+
+    token: HttpUrl
+    """The URL of the authorization endpoint to obtain a token."""
+
+    products: HttpUrl
+    """The endpoint for the products listing."""
+
+
+class RootData(BaseModel):
+    """Sub-resource providing metadata about the service."""
+
+    server_version: str
+    """The service vesion."""
+
+    documentation: HttpUrl
+    """The URL of the service's documentation."""
+
+    message: str
+    """Description of the service."""
+
+
+class RootResponse(BaseModel):
+    """The root endpoint resources provides metadata and links for the
+    service.
+    """
+
+    data: RootData
+
+    links: RootLinks
--- keeper/api/root.py
+++ keeper/api/root.py
@@ -1,31 +1,33 @@
 """Root API route (GET /)."""
 
-from flask import jsonify, url_for
+from flask import url_for
 from flask_accept import accept_fallback
 
 from keeper.api import api
 from keeper.logutils import log_route
 from keeper.version import get_version
 
+from ._models import RootData, RootLinks, RootResponse
+
 
 @api.route("/", methods=["GET"])
 @accept_fallback
 @log_route()
-def get_root() -> None:
+def get_root() -> str:
     """Root API route."""
     version = get_version()
-    data = {
-        "server_version": version,
-        "documentation": "https://ltd-keeper.lsst.io",
-        "message": (
+    root_data = RootData(
+        server_version=version,
+        documentation="https://ltd-keeper.lsst.io",
+        message=(
             "LTD Keeper is the API service for managing LSST the Docs "
             "projects."
         ),
-    }
-
-    links = {
-        "self": url_for("api.get_root", _external=True),
-        "token": url_for("api.get_auth_token", _external=True),
-        "products": url_for("api.get_products", _external=True),
-    }
-    return jsonify({"data": data, "links": links})
+    )
+    links = RootLinks(
+        self=url_for("api.get_root", _external=True),
+        token=url_for("api.get_auth_token", _external=True),
+        products=url_for("api.get_products", _external=True),
+    )
+    response = RootResponse(data=root_data, links=links)
+    return response.json()
